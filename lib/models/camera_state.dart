05c388ba3e3bb86f980041e66dfd07059a5c4837
writepro4 <writepro4@naver.com>
2020-11-24 04:58:57 +0900

    [update] 66 build camera state model(change notifier model)

---
+++ lib/models/camera_state.dart
@@ -0,0 +1,42 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraState extends ChangeNotifier {
+  CameraController _controller;
+  CameraDescription _cameraDescription;
+  bool _readytakePhoto = false;
+
+  void getReadyToTakePhoto() async {
+    List<CameraDescription> cameras = await availableCameras();
+
+    if (cameras != null && cameras.isNotEmpty) {
+      setCameraDescription(cameras[0]);
+    }
+
+    bool init = false;
+    while (!init) {
+      init = await initalize();
+    }
+
+    _readytakePhoto = true;
+    notifyListeners();
+  }
+
+  void setCameraDescription(CameraDescription cameraDescription) {
+    _cameraDescription = cameraDescription;
+    _controller = CameraController(_cameraDescription, ResolutionPreset.medium);
+  }
+
+  Future<bool> initalize() async {
+    try {
+      await _controller.initialize();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  CameraController get controller => _controller;
+  CameraDescription get description => _cameraDescription;
+  bool get isReadyToTakePhoto => _readytakePhoto;
+}
